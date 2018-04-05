bb1597ce33eb184800ebbdef407409e23c2abc94
Marcos Ostos <msanchez@qdqmedia.com>
2018-04-05 16:35:07 +0200

    The hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
